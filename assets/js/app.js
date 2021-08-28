2394c83511bfc57968592907d1fb6a6b1f52710f
pipex <ffariascubillos@gmail.com>
2021-08-27 21:55:56 -0400

    segunda subida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-console.log('hola')
+//carga del sitio
+// window.addEventListener('load', function(){
+//     setTimeout(() => {
+//         // alert('Sitio cargado!')
+//         $('.loader').addClass('off-loader')  
+//     }, 3000)
+// })
+
+AOS.init() // Inicia plugin AOS.
+
+$('li a').on('click', function(){   // Agrega class active al tag <a>.
+    $('li a').removeClass('active')
+    $(this).addClass('active')
+})
+
+var url = window.location.href
+console.log('la url actual es: ' + url)
+
